d6a8dedd8f88f4ca959089532aa31fe7fadcd9f3
HyttalloCarneiro <hc.periciamedica@hotmail.com>
2025-07-12 11:02:09 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import json
 import locale
 
 # Ajuste dos imports dos módulos das páginas
-from pages.laudos_ad import gerar_laudos_ad
+from pages.laudos_ad import gerar_laudo_ad
 
 # Configuração da página
 st.set_page_config(
