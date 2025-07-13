c0be5b880ec4c1752a0525b5700260b24fcd93fa
HyttalloCarneiro <hc.periciamedica@hotmail.com>
2025-07-12 22:21:52 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), "pages"))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'pages'))
 import streamlit as st
 import fitz  # PyMuPDF
 import pandas as pd
